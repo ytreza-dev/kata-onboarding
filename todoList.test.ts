c45799900bf1776495489e4eebd4e72ff58fe0c3
Jonathan LAURENT <johjo@ytreza.org>
2025-01-16 15:58:04 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:todoList.test.ts

--- todoList.test.ts
+++ todoList.test.ts
@@ -1,21 +1,63 @@
+// Quand on ajoute une tâche, la première lettre de la tâche doit être mis en majuscule et doit se finir par un point.
 
-class TodoList {
+// add buy the milk -> Buy the milk.
+// add Buy the water -> Buy the water.
+
+class RepositoryForTest {
     constructor() {
-        this.todos = [];
+        this.tasks = [];
     }
 
-    add(item) {
-        this.todos.push(item);
+    push(task) {
+        this.tasks.push(task);
+    }
+}
+
+class AddTaskUseCase {
+    constructor(repository) {
+        this.repository = repository;
+    }
+
+    execute(task) {
+        let taskWithUpperCaseFirstLetter = this.formatTask(task);
+        this.repository.push({name: taskWithUpperCaseFirstLetter});
+    }
+
+    formatTask(task) {
+        let taskWithUpperCaseFirstLetter = task.charAt(0).toUpperCase() + task.slice(1);
+        if (taskWithUpperCaseFirstLetter.charAt(taskWithUpperCaseFirstLetter.length - 1) !== ".") {
+            taskWithUpperCaseFirstLetter += ".";
+        }
+        return taskWithUpperCaseFirstLetter;
     }
 }
 
 describe('todo list', () => {
-    it('should add a todo', () => {
-        const todo = new TodoList();
+    it('should add a task', () => {
+        const repository = new RepositoryForTest();
 
-        todo.add({name: "buy milk"});
+        const useCase = new AddTaskUseCase(repository);
+        useCase.execute("Buy the egg.");
 
-        expect(todo.todos.length).toBe(1);
-        expect(todo.todos[0].name).toBe("buy milk");
+        expect(repository.tasks).toEqual([{name: "Buy the egg."}])
     });
+
+    it('should task have a upper case first letter', () => {
+        const repository = new RepositoryForTest();
+
+        const useCase = new AddTaskUseCase(repository);
+        useCase.execute("buy the egg.");
+
+        expect(repository.tasks).toEqual([{name: "Buy the egg."}])
+    });
+
+    it('should task have a point at the end', () => {
+        const repository = new RepositoryForTest();
+
+        const useCase = new AddTaskUseCase(repository);
+        useCase.execute("Buy the egg");
+
+        expect(repository.tasks).toEqual([{name: "Buy the egg."}])
+    });
+
 });
