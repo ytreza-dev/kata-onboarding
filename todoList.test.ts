1471404a175e824148be1acb26d89f63059ccb38
Jonathan LAURENT <johjo@ytreza.org>
2025-01-16 14:55:31 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:todoList.test.ts

--- todoList.test.ts
+++ todoList.test.ts
@@ -1,5 +1,19 @@
+class TodoList {
+    constructor() {
+        this.todos = [];
+    }
+
+    add(buyMilk) {
+        this.todos.push(buyMilk);
+    }
+}
+
 describe('todo list', () => {
     it('should add a todo', () => {
-        expect('a').toEqual('b');
+        const todo = new TodoList();
+
+        todo.add("buy milk");
+
+        expect(todo.todos.length).toBe(1);
     });
 });
