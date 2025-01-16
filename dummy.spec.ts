f6e077c96d4dd979724b09db3399c9e699a23807
David Robert <contact@david-robert.fr>
2025-01-16 10:34:12 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:dummy.spec.ts

--- dummy.spec.ts
+++ dummy.spec.ts
@@ -1,5 +1,9 @@
-describe('tto', () => {
-  it('it', () => {
-    expect(true).toBe(true);
-  })
-})
+describe("tto", () => {
+  it("it should print 1 when the number is 1", () => {
+    const number = 1;
+    expect(`${number}`).toBe("1");
+  });
+});
+
+// driver = clavier
+// navigator = "dirige"
